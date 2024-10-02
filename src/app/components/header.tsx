a75e983ceca658ca65eb22cdc1e66e467d915053
pixbs <hey@pixbs.com>
2024-10-02 02:19:08 +0200

    🍋‍🟩 chores: Add husky and pre-commit command for linting 🔢 feat: Add stats section

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -8,12 +8,12 @@ export default function Header() {
 				className='h-tile w-tile rounded-full bg-brand'
 				aria-label='Return home'
 			/>
-			<button className='flex size-tile flex-col items-center justify-center gap-quarter rounded-full border border-backgroundSecondary bg-background'>
+			<button className='peer flex size-tile flex-col items-center justify-center gap-quarter rounded-full border border-backgroundSecondary bg-background'>
 				<span className='sr-only'>Menu</span>
-				<div className='gap-2'>
+				<div className='gap-2' aria-label='Open menu'>
+					<hr className='w-half peer-hover:w-quarter' />
 					<hr className='w-half' />
-					<hr className='w-half' />
-					<hr className='w-quarter' />
+					<hr className='w-quarter peer-hover:w-half' />
 				</div>
 			</button>
 		</header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Hero from './components/hero'
+import Stats from './components/stats'
 
 export default function Home() {
-	return <Hero />
+	return (
+		<>
+			<Hero />
+			<Stats />
+		</>
+	)
 }
