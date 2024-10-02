2533b297fc7cbb5779c0c61914c8f03feef8280a
pixbs <hey@pixbs.com>
2024-10-02 02:34:04 +0200

    🔨 fix: Resolved eslint issues

--- src/app/components/stats.tsx
+++ src/app/components/stats.tsx
@@ -1,19 +1,19 @@
 export default function Stats() {
 	return (
 		<section>
-			<div className="px-tile gap-tile py-tile">
+			<div className='gap-tile px-tile py-tile'>
 				<StatsCard
 					number='90+'
 					description='Successful products created by me'
 				/>
-                <StatsCard
-                    number='8'
-                    description='Years of commercial experience '
-                />
-                <StatsCard
-                    number='99%'
-                    description='Client satisfaction on final work by me'
-                />
+				<StatsCard
+					number='8'
+					description='Years of commercial experience '
+				/>
+				<StatsCard
+					number='99%'
+					description='Client satisfaction on final work by me'
+				/>
 			</div>
 		</section>
 	)
@@ -27,12 +27,12 @@ interface StatsCardProps {
 function StatsCard(props: StatsCardProps) {
 	const { number, description } = props
 	return (
-		<div className="bg-background h-quad px-half w-full border-b border-l border-backgroundSecondary">
-            <div className="md:h-double justify-center">
-			    <h2 className='headline'>{number}</h2>
-            </div>
-            <hr className="border-backgroundSecondary"/>
-			<p className="pt-half">{description}</p>
+		<div className='h-quad w-full border-b border-l border-backgroundSecondary bg-background px-half'>
+			<div className='justify-center md:h-double'>
+				<h2 className='headline'>{number}</h2>
+			</div>
+			<hr className='border-backgroundSecondary' />
+			<p className='pt-half'>{description}</p>
 		</div>
 	)
 }
