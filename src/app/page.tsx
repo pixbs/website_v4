facaa3c9c46b04f6eb7cb83c6c5f0d4b20d704df
pixbs <hey@pixbs.com>
2024-10-10 05:35:51 +0200

    🚩 feat: Meta adjustments, added favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Metadata } from 'next'
 import Hero from './components/hero'
 import Stats from './components/stats'
-import Works from './components/works'
 import Loading from './loading'
 
 export const metadata: Metadata = {
 	title: "Welcome to Dimm's website!",
 	description:
-		'Dimm&apos;s portfolio website showcasing his works and skills',
+		"Dimm's portfolio website showcasing his works and skills",
 }
 
 export default function Home() {
@@ -17,7 +16,7 @@ export default function Home() {
 			<main>
 				<Hero />
 				<Stats />
-				<Works />
+				{/* <Works /> */}
 			</main>
 		</>
 	)
